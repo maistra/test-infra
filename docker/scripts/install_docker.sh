f971e9dfa961d60981808f482371c50bbb3c498d
Daniel Grimm <dgrimm@redhat.com>
2020-09-29 09:49:34 +0200

    Fix 1.1 container build

--- docker/scripts/install_docker.sh
+++ docker/scripts/install_docker.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-DOCKER_VERSION=19.03.8
+DOCKER_VERSION="19.03*"
 
 yum install -y yum-utils device-mapper-persistent-data lvm2
 
@@ -13,4 +13,4 @@ yum-config-manager \
 # we have to manually install containerd.io to resolve a conflict
 yum install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.2-3.3.el7.x86_64.rpm
 
-yum install -y docker-ce-${DOCKER_VERSION} docker-ce-cli-${DOCKER_VERSION}
+yum install -y "docker-ce-${DOCKER_VERSION}" "docker-ce-cli-${DOCKER_VERSION}"
