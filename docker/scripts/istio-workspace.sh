5321d129f599e94f11065d348612d6f851253228
Bartosz Majsak <bartosz.majsak@gmail.com>
2020-11-26 20:45:08 +0100

    chore: cleans up bash script

--- docker/scripts/istio-workspace.sh
+++ docker/scripts/istio-workspace.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 set -e
-#set -o xtrace
 
-export IKE_IMAGE_TAG="PR-$PULL_NUMBER"
+export IKE_IMAGE_TAG="PR-${PULL_NUMBER}"
 
 export GOPATH="`pwd`/"
-export GOBIN="$GOPATH/bin"
+export GOBIN="${GOPATH}/bin"
 export GOROOT=/usr/lib/golang/
-export PATH="$PATH:$GOPATH/bin"
+export PATH="${PATH}:${GOPATH}/bin"
 
-mkdir -p $GOPATH/src/github.com/$REPO_OWNER/
-cd $GOPATH/src/github.com/$REPO_OWNER/
-git clone https://github.com/$REPO_OWNER/$REPO_NAME.git 
-cd $REPO_NAME
-git checkout $PULL_PULL_SHA
+mkdir -p "${GOPATH}"/src/github.com/"${REPO_OWNER}"/
+cd "${GOPATH}"/src/github.com/"${REPO_OWNER}"/
+git clone https://github.com/"${REPO_OWNER}"/"${REPO_NAME}".git
+cd "${REPO_NAME}"
+git checkout "${PULL_PULL_SHA}"
 
-oc login $IKE_CLUSTER_ADDRESS -u $IKE_CLUSTER_USER -p $IKE_CLUSTER_PWD --insecure-skip-tls-verify=true
+oc login "${IKE_CLUSTER_ADDRESS}" -u "${IKE_CLUSTER_USER}" -p "${IKE_CLUSTER_PWD}" --insecure-skip-tls-verify=true
+
 make deps tools test-e2e
